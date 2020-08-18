ce7f0756ac600b79b4b5d7e997c22379237b2e96
Gustav Sundin <sundin666@gmail.com>
2020-08-18 16:44:07 +0200

    Store events in DynamoDB

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,35 @@
-console.log('Loading function');
-
 var AWS = require('aws-sdk');
-//var dynamo = new AWS.DynamoDB.DocumentClient();
+AWS.config.update({region: process.env.AWS_REGION});
 
-exports.handler = function(event, context, callback) {
+var ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async function(event, context, callback) {
     console.log('Received body:', JSON.stringify(event.body, null, 2));
 
-    let responseBody = {
-        message: "success"
-    };
-    
-    let response = {
-        statusCode: 200,
-        body: JSON.stringify(responseBody)
+    var params = {
+        TableName: process.env.DynamoTableName,
+        Item: {
+            'id': uuidv4(),
+            'events': JSON.parse(event.body)
+        }
     };
 
-    console.log("response: " + JSON.stringify(response));
-
-    callback(null, response);
+    return new Promise((resolve, reject) => {
+        ddb.put(params, function(err, data) {
+            if (err) {
+                console.log("Error", err);
+                reject(Error(err));
+            } else {
+                console.log("Success", data);
+                resolve(200);
+            }
+        });
+    });
 };
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
