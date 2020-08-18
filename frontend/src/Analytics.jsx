daa2d159e4ca24c35b86a11849927cd534a5704c
Rickard Andersson <dunklas@gmail.com>
2020-08-18 14:51:36 +0200

    Use ref instead of local state
    Due to problems accessing state from event listeners

--- frontend/src/Analytics.jsx
+++ frontend/src/Analytics.jsx
@@ -1,18 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 const Analytics = () => {
 
-    const [events, setEvents] = useState([]);
+    const eventsRef = React.useRef([]);
+
+    const pushEvent = event => {
+        eventsRef.current = [
+            ...eventsRef.current,
+            event,
+        ];
+    }
+
+    let skip;
+    const endSession = () => {
+        if (skip) return
+        skip = true
+        console.log(eventsRef.current);
+        debugger;
+    }
+
+    React.useEffect(() => {
+        window.addEventListener('pagehide', endSession);
+        window.addEventListener('beforeunload', endSession)
+        window.addEventListener('unload', endSession)
+    }, []);
 
     const location = useLocation();
     React.useEffect(() => {
-        setEvents(events => ([
-            ...events,
-            {
-                pathname: location.pathname
-            }
-        ]));
+        pushEvent({
+            pathname: location.pathname
+        });
         console.log('Location changed', location.pathname);
     }, [location]);
 
