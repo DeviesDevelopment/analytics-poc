396e6e9d37b08aef9e5a422ac715afe20b43199f
Rickard Andersson <dunklas@gmail.com>
2020-08-18 13:56:03 +0200

    Add some local state to Analytics

--- frontend/src/Analytics.jsx
+++ frontend/src/Analytics.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const Analytics = () => {
 
+    const [events, setEvents] = useState([]);
+
     const location = useLocation();
     React.useEffect(() => {
+        setEvents(events => ([
+            ...events,
+            {
+                pathname: location.pathname
+            }
+        ]));
         console.log('Location changed', location.pathname);
     }, [location]);
 
