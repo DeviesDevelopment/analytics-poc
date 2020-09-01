2ed6677d0c970942d5670d1bf330d63ebc32a389
Gustav Sundin <sundin666@gmail.com>
2020-09-01 11:10:05 +0200

    Typo

--- frontend/src/UserAnalytics.jsx
+++ frontend/src/UserAnalytics.jsx
@@ -4,7 +4,7 @@ const UserAnalytics = () => {
 
     const [analytics, setAnalytics] = useState([]);
     useEffect(() => {
-        fetch(REACT_APP_BACKEND_URL + '/analytics')
+        fetch(process.env.REACT_APP_BACKEND_URL + '/analytics')
             .then(response => response.json())
             .then(response => {
                 setAnalytics(response);
