8949c6bcf720a001860c0d7ad1a3b45c6ff40914
Gustav Sundin <sundin666@gmail.com>
2020-09-01 11:32:51 +0200

    Some nice tabs and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
-import TrustStore from './TrustStore';
+import PageVisits from './pages/PageVisits';
+import Sessions from './pages/Sessions';
+import UserAgents from './pages/UserAgents';
 import Analytics from './Analytics';
-import UserAnalytics from './UserAnalytics';
 import './index.css';
 
 const App = () => {
@@ -11,14 +12,18 @@ const App = () => {
       <nav className="navbar navbar-light">
         <ul className="nav navbar-nav">
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/user-analytics">User Analytics</Link></li>
+          <li><Link to="/sessions">Sessions</Link></li>
+          <li><Link to="/page-visits">Page Visists</Link></li>
+          <li><Link to="/user-agents">User Agents</Link></li>
         </ul>
       </nav>
 
       <Analytics />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/user-analytics" component={UserAnalytics} />
+        <Route path="/sessions" component={Sessions} />
+        <Route path="/page-visits" component={PageVisits} />
+        <Route path="/user-agents" component={UserAgents} />
       </Switch>
 
     </div>
@@ -30,6 +35,11 @@ const Home = (props) => (
     <h2>Home</h2>
     <p>Welcome stranger!</p>
     <p>This page collects usage behavior data.<br />Browse the tabs above to view the collected statistics aggregated in various ways.</p>
+    <p>Feel free to browser around for a while and view this month's collected data using the tabs above. 
+      As soon as you finish your browser session (by closing this tab, refreshing the page or navigating to a different website), 
+      details about your session will be sent to our backend and stored in database.
+      If you come back here again you will therefore notice that the numbers have changed.
+      No personal data is gathered this way.</p>
     <p>Source code available here: <a href="https://github.com/DeviesDevelopment/analytics-poc">https://github.com/DeviesDevelopment/analytics-poc</a></p>
   </div>
 );
---
+++ frontend/src/pages/UserAgents.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+const UserAnalytics = () => {
+
+    const [analytics, setAnalytics] = useState([]);
+    useEffect(() => {
+        fetch(process.env.REACT_APP_BACKEND_URL + '/analytics')
+            .then(response => response.json())
+            .then(response => {
+                setAnalytics(response);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h2>User agents</h2>
+            <p>These are the User-Agent headers sent by our clients. The data here can be used to find out which devices and browsers that are commonly used to view our site.</p>
+            <p>User agents:</p>
+            <ul>
+                {usedBrowsers(analytics).map(userAgent => <li key={userAgent.browser}>{userAgent.browser}: {userAgent.visits}</li>)}
+            </ul>
+        </div>
+    );
+}
+
+const usedBrowsers = (analytics) => {
+    const browsers = analytics
+        .map(analytic => analytic.browser)
+        .reduce((totals, current) => {
+            if (!totals[current]) {
+                totals[current] = 0;
+            }
+            totals[current] = totals[current] + 1
+            return totals;
+        }, {});
+
+    return Object.keys(browsers)
+        .map(browser => ({
+            browser,
+            visits: browsers[browser]
+        }))
+        .sort((a, b) => b.visits - a.visits)
+}
+
+export default UserAnalytics;
