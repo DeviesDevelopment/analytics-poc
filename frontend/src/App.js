5a7f9eca702a0134a7620d6935ab3b2ad0b8c7fb
Gustav Sundin <sundin666@gmail.com>
2020-09-01 11:13:46 +0200

    Remove empty tabs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,11 @@ import UserAnalytics from './UserAnalytics';
 import './index.css';
 
 const App = () => {
-
   return (
     <div>
       <nav className="navbar navbar-light">
         <ul className="nav navbar-nav">
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/api-docs">API Docs</Link></li>
-          <li><Link to="/trust-store">Trust Store</Link></li>
-          <li><Link to="/vehicle-data">Vehicle Data</Link></li>
-          <li><Link to="/vehicle-gate-log">Vehicle Gate Log</Link></li>
           <li><Link to="/user-analytics">User Analytics</Link></li>
         </ul>
       </nav>
@@ -23,45 +18,20 @@ const App = () => {
       <Analytics />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/api-docs" component={ApiDocs} />
-        <Route path="/trust-store" component={TrustStore} />
-        <Route path="/vehicle-data" component={VehicleData} />
-        <Route path="/vehicle-gate-log" component={VehicleGateLog} />
         <Route path="/user-analytics" component={UserAnalytics} />
       </Switch>
 
     </div>
   );
-}
+};
 
 const Home = (props) => (
   <div>
     <h2>Home</h2>
-    <p>Welcome to the fake DPAP!</p>
-    <p>This page collects usage behavior data.<br />Go to <strong>User Analytics</strong> tab to view.</p>
+    <p>Welcome stranger!</p>
+    <p>This page collects usage behavior data.<br />Browse the tabs above to view the collected statistics aggregated in various ways.</p>
+    <p>Source code available here: <a href="https://github.com/DeviesDevelopment/analytics-poc">https://github.com/DeviesDevelopment/analytics-poc</a></p>
   </div>
-)
-
-const ApiDocs = () => (
-  <div>
-    <h2>API Docs</h2>
-    <p>API Docs stuff</p>
-  </div>
-)
-
-const VehicleGateLog = () => (
-  <div>
-    <h2>Vehicle Gate Log</h2>
-    <p>Vehicle Gate Log stuff</p>
-  </div>
-)
-
-const VehicleData = () => (
-  <div>
-    <h2>Vehicle Data</h2>
-    <p>Vehicle Data stuff</p>
-  </div>
-)
-
+);
 
 export default App;
