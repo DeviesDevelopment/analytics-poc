93b0fbbe03de3aca94daed5ac9c6b20e12096687
Gustav Sundin <sundin666@gmail.com>
2020-09-01 13:03:16 +0200

    Average time spent per page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Link, Route, Switch } from 'react-router-dom';
 import PageVisits from './pages/PageVisits';
 import Sessions from './pages/Sessions';
 import UserAgents from './pages/UserAgents';
+import TimeSpent from './pages/TimeSpent';
 import Analytics from './Analytics';
 import './index.css';
 
@@ -15,6 +16,7 @@ const App = () => {
           <li><Link to="/sessions">Sessions</Link></li>
           <li><Link to="/page-visits">Page Visists</Link></li>
           <li><Link to="/user-agents">User Agents</Link></li>
+          <li><Link to="/time-spent">Time Spent</Link></li>
         </ul>
       </nav>
 
@@ -24,6 +26,7 @@ const App = () => {
         <Route path="/sessions" component={Sessions} />
         <Route path="/page-visits" component={PageVisits} />
         <Route path="/user-agents" component={UserAgents} />
+        <Route path="/time-spent" component={TimeSpent} />
       </Switch>
 
     </div>
@@ -34,7 +37,7 @@ const Home = (props) => (
   <div>
     <h2>Home</h2>
     <p>Welcome stranger!</p>
-    <p>This page collects usage behavior data.<br />Browse the tabs above to view the collected statistics aggregated in various ways.</p>
+    <p>This page is a demonstration of that it is possible to collect and aggregate user analytics without using any third-party tools.</p>
     <p>Feel free to browser around for a while and view this month's collected data using the tabs above. 
       As soon as you finish your browser session (by closing this tab, refreshing the page or navigating to a different website), 
       details about your session will be sent to our backend and stored in database.
